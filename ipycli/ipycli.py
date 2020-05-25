d0730c85bb34b19a95c39be533bf943eaae35435
kirankotari <kirankotari@live.com>
2020-05-25 19:03:51 +0530

    initial files

---
+++ ipycli/ipycli.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env ipython
+from __future__ import print_function
+import ncs
+import IPython
+
+
+def run():
+    IPython.get_ipython().define_macro('variables', """a=10
+                                                       b=20
+                                                        print("new variables created")
+                                                    """)
+
+    print("Your new variables are a=10, b=20")
+    print("""This is my new Banner
+don't change the banner..!
+""")
+    IPython.embed(display_banner=False)
+
+if __name__ == '__main__':
+    run()
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+reqs = []
+
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    read_lines = f.readlines()
+    reqs = [each.strip() for each in read_lines]
+
+setup(
+    name = 'ipycli',
+    version = '1.0.0',
+    description = "Gives you an interactive ipython shell with tab completion.",
+    long_description = '''long_description''',
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/kirankotari/ipycli.git',
+    author = 'Kiran Kumar Kotari',
+    author_email = 'kirankotari@live.com',
+    entry_points={
+        'console_scripts': [
+            'ipycli=ipycli.ipycli:run'
+        ],
+    },
+    install_requires=reqs,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: IPython'
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    keywords = 'ncs-pycli ncs_pycli',
+    packages = find_packages(where='.', exclude=['tests']),
+    include_package_data=True,
+)
+
